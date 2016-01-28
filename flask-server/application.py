82fa155995611aaae75e232474f0df837d44aa29
Daniel Jonsson <wd.jonsson@gmail.com>
2016-01-28 15:37:18 +0100

    More stuff

--- flask-server/application.py
+++ flask-server/application.py
@@ -1,5 +1,5 @@
 import json
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, redirect
 import time
 from threading import Thread
 
@@ -7,7 +7,16 @@ application = Flask(__name__)
 
 main_route = '/flask-debug-server'
 
+@application.route('/')
+def new_root():
+    return redirect(main_route, code=302)
+
 @application.route('/healthcheck')
+def healthcheck():
+    return redirect(main_route + '/healthcheck', code=302)
+
+
+@application.route(main_route + '/healthcheck')
 def health_check():
     return jsonify(application='load-server',
                    status='OK',
@@ -15,26 +24,30 @@ def health_check():
                    version='0.1',
                    NODE_ENV='python :)')
 
-
 @application.route(main_route + '/debug', methods=['GET', 'POST', 'PATCH', 'PUT', 'DELETE'])
 def debug():
-    sleep_in_ms = request.args.get('time')
-    sleep_in_seconds = float(sleep_in_ms) / 1000
-    Sleep(sleep_in_seconds).start()
-    r_code = int(request.args.get('r_code'))
-    response = jsonify(response_time=sleep_in_ms, request_method=request.method, response_code=r_code)
-    response.status_code = r_code
-    return response
+    try:
+        sleep_in_ms = request.args.get('time')
+        sleep_in_seconds = float(sleep_in_ms) / 1000
+        Sleep(sleep_in_seconds).start()
+        r_code = int(request.args.get('r_code'))
+        response = jsonify(response_time=sleep_in_ms, request_method=request.method, response_code=r_code)
+        response.status_code = r_code
+        return response
+    except:
+        return error_handler('Time and/or r_code is missing')
 
 
 @application.route(main_route + '/json_in_json_out', methods=['GET', 'POST'])
 def json_in_json_out():
     if request.method == 'POST':
         try:
-            json_data = json.loads(request.data)
-            return jsonify(response=json_data)
+            persisted = json_data = json.loads(request.data)
+            return jsonify(response=persisted)
         except:
             return error_handler('Unable to parse json')
+    if request.method == 'GET':
+        return error_handler('Do a POST!')
 
 
 @application.route(main_route + '/')
@@ -53,5 +66,5 @@ class Sleep(Thread):
         time.sleep(sleep_in_seconds)
 
 if __name__ == '__main__':
-    # application.run(port=8080, debug=True)
-    application.run('0.0.0.0')
+     application.run(port=8080, debug=True)
+    #application.run('0.0.0.0')
